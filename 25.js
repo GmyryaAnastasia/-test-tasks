309e0c8ca7c26a6967e8e80808ed40de80608b15
Nastya <nastgm26@gmail.com>
2022-03-27 17:01:30 +0300

    25-29 add 30

--- 25.js
+++ 25.js
@@ -8,3 +8,8 @@ const myStr = "This is the start. " + "This is the end."; // Change this line
 
 let myStr = "This is the first sentence. ";
 myStr +="This is the second sentence."
+
+
+// Only change code below this line
+const myName = "Anastasia";
+const myStr = "My name is " + myName + "and I am well!";
