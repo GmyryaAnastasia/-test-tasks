1a4a0df3f29ca4c04439df75a05fb22d3d86dd6a
Nastya <nastgm26@gmail.com>
2022-03-28 05:24:00 +0300

    41-44 ad 45

--- 41.js
+++ 41.js
@@ -24,3 +24,10 @@ const myArray = [["John", 23], ["cat", 2]];
 
 // Only change code below this line
 myArray.push(["daog",3]);
+
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+const removedFromMyArray = myArray.pop()
