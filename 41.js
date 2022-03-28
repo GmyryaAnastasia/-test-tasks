0b7a7b97f3d58437724c108672e55d5c13886042
Nastya <nastgm26@gmail.com>
2022-03-28 05:32:59 +0300

    41-46 ad 47

--- 41.js
+++ 41.js
@@ -38,3 +38,11 @@ const myArray = [["John", 23], ["dog", 3]];
 
 // Only change code below this line
 let removedFromMyArray = myArray.shift();
+
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul",35]);
