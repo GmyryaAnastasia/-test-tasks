f315737cfd6e354110f778bd09d4c3c406fa5fc4
Nastya <nastgm26@gmail.com>
2022-09-04 19:43:04 +0300

    85

---
+++ 85.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    name: "Kokos",
+    tails: 1,
+    legs: 4,
+    friends: ["Nastya", "Masha"]
+
+    // Only change code above this line
+};
